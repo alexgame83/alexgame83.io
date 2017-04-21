24300ef6fd90b4225ef6b8cd156f224ef5cf1dae
alexgame83 <alexgame83@yahoo.com>
2017-04-21 11:01:07 +0200

    first page

---
+++ Fd.js
@@ -0,0 +1,19 @@
+function changeText(id) {
+ id.innerHTML = "Ooops!" 
+}
+
+
+document.getElementById("myBtn").addEventListener("click", myFunction);  
+function myFunction() {
+  alert("We apologize this option is currently broken");
+}
+
+document.getElementById("myTopnav").addEventListener("click", myFunction);
+function myFunction() {
+  alert("We apologize this option is currently broken");
+}
+document.getElementById("logo").addEventListener("click", myFunction);
+function myFunction() {
+  alert("We apologize this option is currently broken");
+}
+
